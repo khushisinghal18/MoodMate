7f8e2161c3a0138f68ee26b82c442f8fc53c4824
khushisinghal18 <singhal.khushi.1.8.8.2.5@gmail.com>
2025-07-02 16:50:14 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MoodMate"> {/* 👈 Important fix */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
